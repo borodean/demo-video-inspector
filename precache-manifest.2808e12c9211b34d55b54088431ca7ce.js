6b417aa555e0d3a3367bdeb14477b66a98f444c9
Vadym Borodin <borodean@gmail.com>
2020-04-01 08:58:17 +0300

    Updates

---
+++ precache-manifest.2808e12c9211b34d55b54088431ca7ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dfb63b218a1ba276583ce38220012a",
+    "url": "/demo-video-inspector/index.html"
+  },
+  {
+    "revision": "f9623d719d403d473b03",
+    "url": "/demo-video-inspector/static/css/2.8dd6f7e2.chunk.css"
+  },
+  {
+    "revision": "1add7f7c13000056ad83",
+    "url": "/demo-video-inspector/static/css/main.fa9ee6d7.chunk.css"
+  },
+  {
+    "revision": "f9623d719d403d473b03",
+    "url": "/demo-video-inspector/static/js/2.384db1fd.chunk.js"
+  },
+  {
+    "revision": "551da903960f7af40b576c201119cd35",
+    "url": "/demo-video-inspector/static/js/2.384db1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1add7f7c13000056ad83",
+    "url": "/demo-video-inspector/static/js/main.37092ffd.chunk.js"
+  },
+  {
+    "revision": "1710d250b2c9d27314a3",
+    "url": "/demo-video-inspector/static/js/runtime-main.e39c39ce.js"
+  }
+]);
