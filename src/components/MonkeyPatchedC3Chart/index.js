6c2d9bd8025b71f608190edf45ed1b21a7167a1f
Vadym Borodin <borodean@gmail.com>
2020-04-01 07:56:55 +0300

    Monkey-patch C3Chart so it properly updates regions

---
+++ src/components/MonkeyPatchedC3Chart/index.js
@@ -0,0 +1,20 @@
+import C3Chart from 'react-c3js';
+
+// Fixes the issue:
+// https://github.com/bcbcarl/react-c3js/issues/18
+
+class MonkeyPatchedC3Chart extends C3Chart {
+  updateChart(config) {
+    super.updateChart(config);
+
+    if (config.regions) {
+      this.chart.regions(config.regions);
+    } else if (this.chart.regions && !config.regions) {
+      this.chart.regions([]);
+    }
+
+    super.loadNewData(config.data);
+  }
+}
+
+export default MonkeyPatchedC3Chart;
